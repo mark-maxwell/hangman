9c286e4619187ec050dc5f413c4f6831810321d1
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-10-22 11:06:43 +0100

    Added step instructions in cucumber

---
+++ features/step_definitions/hangman_steps.rb
@@ -0,0 +1,48 @@
+ 
+Given(/^a valid guess$/) do
+  pending
+end
+
+When(/^a user makes a guess$/) do
+  pending
+end
+
+Then(/^user is told guess is correct$/) do
+  pending
+end
+
+Then(/^word is updated to include new guess$/) do
+  pending
+end
+
+Given(/^an invalid guess$/) do
+  pending
+end
+
+Then(/^user is told guess is incorrect$/) do
+  pending
+end
+
+Then(/^a life is deducted$/) do
+  pending
+end
+
+When(/^there is only one letter left to guess$/) do
+  pending
+end
+
+Then(/^user is told they have guessed the word$/) do
+  pending
+end
+
+Then(/^the game is over$/) do
+  pending
+end
+
+When(/^user only has one life left$/) do
+  pending
+end
+
+Then(/^the user is told they have ran out of lives$/) do
+  pending
+end
