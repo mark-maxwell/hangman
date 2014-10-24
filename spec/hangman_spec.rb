4ac07064a30d8a0c876da64fb98d0d24f25f5874
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-10-24 09:15:56 +0100

    Added rspec tests for 'trash'

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -19,13 +19,25 @@ describe "user makes a guess" do
     hangman.check_guess(guess)
   end
 
+  it "can add an invalid guess to trash" do
+    expect(display).to receive(:invalid_guess)
+    hangman.check_guess("p")
+    expect(hangman.get_trashed_letters).to eq(["p"])
+  end
+
+  it "can use trash to keep track of multiple invalid guesses" do
+    expect(display).to receive(:invalid_guess).twice
+    hangman.check_guess("q")
+    hangman.check_guess("w")
+    expect(hangman.get_trashed_letters).to eq(["q", "w"])
+  end
+
 describe "lives can be checked and deducted" do
   it "can remove a life" do
     expect(display).to receive(:invalid_guess)
     current_lives = hangman.player.lives
     hangman.check_guess("r")
-    expect(current_lives - 1 ).to eq(hangman.player.lives)
+    expect(current_lives - 1).to eq(hangman.player.lives )
   end
 end
-
 end
