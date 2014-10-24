0ea4fa9e5c0955d05dce94742b073d754ad3f97b
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-10-24 11:51:35 +0100

    Horrible implimentation of front-end display code - Really needs refactoring

--- hangman_app.rb
+++ hangman_app.rb
@@ -13,8 +13,31 @@ get '/' do
   if params[:guess]
     hangman_game.check_guess(params[:guess])
   end
+
+  game_over = "hidden"
+
+  if hangman_game.display.game_complete
+    game_over_input = "disabled"
+    game_over = "game-over"
+    game_over_message = ""
+    if hangman_game.display.last_guess_status == "valid"
+      game_over_message = "Well done!"
+    else
+      game_over_message = "You lose!"
+    end
+  end
+
   erb :hangman, :locals => { 
     :lives => hangman_game.player.lives, 
-    :progress => hangman_game.display_progress 
+    :progress => hangman_game.display_progress,
+    :game_over => game_over,
+    :game_over_input => game_over_input,
+    :game_over_message => game_over_message
   }
+
+  # if !hangman_game.display.game_complete
+  #   erb :hangmaninput
+  # else
+  #   'You loose'
+  # end
 end
