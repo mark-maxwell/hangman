9f671b9d83d6ae1feb736efc994d18622c572906
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-10-29 15:28:39 +0000

    A set of small changes
    * Reversed a change in word generator (was changed for testing purposes)
    * Updated validator to treat white space the same as entering nothing for a guess
    * Added a new game button to webapp
    * Added some CSS styling to new button and existing button, plus some other slight visual changes.

--- lib/validate.rb
+++ lib/validate.rb
@@ -5,9 +5,13 @@ class Valid
     elsif trash.include?(guess.downcase)
       return [false, "Too slow on the draw partner."]
     elsif correct_guesses.include?(guess.downcase)
-      return false, "C'mon now!!! You just tried that... LOOK UP!!!"
+      return [false, "C'mon now!!! You just tried that... LOOK UP!!!"]
+    elsif guess == "" || guess =~ /[\s]/
+      return [false, "Maybe try entering something!?!?!?"]
     else
       return [true, ""]
     end
   end
 end
+
+#new error for when a blank guess is made "". maybe change trash error message
